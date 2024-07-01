ac15c0a1ca4062e423e3fc4d831de13d79b8ee5e
Bimzy <branden.immerzeel@gmail.com>
2024-07-01 18:52:34 +0930

    ReorderRoutesMakeAllPathsLeadCityZero

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,6 @@
-from Medium.KeysAndRooms import KeysAndRooms
-from Medium.NumberOfProvinces import NumberOfProvinces
+from Medium.ReorderRoutesMakeAllPathsLeadCityZero import ReorderRoutesMakeAllPathsLeadCityZero
 from TreeNode import TreeNodeHelper
 
 if __name__ == '__main__':
-    main = NumberOfProvinces([[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]])
+    main = ReorderRoutesMakeAllPathsLeadCityZero(5, [[4,3],[2,3],[1,2],[1,0]])
+    main = ReorderRoutesMakeAllPathsLeadCityZero(5, [[1,0],[1,2],[3,2],[3,4]])
