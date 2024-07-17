35909a950d9f5d2d912be1642554b073d5cd8560
Bimzy <branden.immerzeel@gmail.com>
2024-07-17 17:11:24 +0930

    SmallestInfiniteSet

--- pythonProject/Medium/SmallestInfiniteSet.py
+++ pythonProject/Medium/SmallestInfiniteSet.py
@@ -1,20 +1,13 @@
 from queue import PriorityQueue
 
 class SmallestInfiniteSet:
-    infSet = PriorityQueue()
     def __init__(self):
-        self.infSet = PriorityQueue()
-        for i in range(int.ma):
-            self.infSet.put(i + 1)
+        nums = list(range(1, 1001))
+        self.sNums = set(nums)
     def popSmallest(self) -> int:
-        if len(self.infSet.queue) == 0:
-            return 0
-        smallest = self.infSet.get()
-        print("smallest " + str(smallest))
-        return smallest
+        small = min(self.sNums)
+        self.sNums.remove(small)
+        return small
 
     def addBack(self, num: int) -> None:
-        if self.infSet.queue.__contains__(num):
-            return
-        self.infSet.put(num)
-        print(len(self.infSet.queue))
+        self.sNums.add(num)
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,14 +1,6 @@
-from Easy.ConvertSortedArrayToBinarySearchTree import ConvertSortedArrayToBinarySearchTree
-from Easy.MergeSortedArray import MergeSortedArray
-from Easy.MinDepthBinaryTree import MinDepthBinaryTree
-from Easy.PascalsTriangle import PascalsTriangle
-from Easy.PathSum import PathSum
-from Easy.SameTree import SameTree
-from Easy.SymmetricTree import SymmetricTree
-from Medium.FindPeakElement import FindPeakElement
-from Medium.KoKoEatingBananas import KoKoEatingBananas
+from Medium.SmallestInfiniteSet import SmallestInfiniteSet
 from TreeNode import TreeNodeHelper
 
 if __name__ == '__main__':
-    main = ConvertSortedArrayToBinarySearchTree([-10,-3,0,5,9])
+    main = SmallestInfiniteSet([-10,-3,0,5,9])
 
