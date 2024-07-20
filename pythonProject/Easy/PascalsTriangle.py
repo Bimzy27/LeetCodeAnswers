e05abb1d4a920862051bd556d30ae977f5e35eb8
Bimzy <branden.immerzeel@gmail.com>
2024-07-20 22:53:14 +0930

    pascals triangle 2

--- pythonProject/Easy/PascalsTriangle.py
+++ pythonProject/Easy/PascalsTriangle.py
@@ -19,3 +19,20 @@ class PascalsTriangle:
                     row[j + 1] = rows[i - 1][j] + rows[i - 1][j + 1]
 
         return rows
+
+class PascalsTriangleII:
+    def __init__(self, rowIndex: int):
+        LogHelper.PrintInput(rowIndex)
+        LogHelper.PrintOutput(self.getRow(rowIndex))
+
+    def getRow(self, rowIndex: int) -> List[int]:
+        rows: List[List[int]] = []
+        for i in range(rowIndex + 1):
+            row: List[int] = [1 for _ in range(i + 1)]
+            rows.append(row)
+
+            if i > 1:
+                for j in range(i - 1):
+                    row[j + 1] = rows[i - 1][j] + rows[i - 1][j + 1]
+
+        return rows[-1]
