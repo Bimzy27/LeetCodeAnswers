925a560e99cbcc509cbf70955618203ddf20c1da
Bimzy <branden.immerzeel@gmail.com>
2024-07-24 15:03:16 +0930

    Followed along with NeetCode, MaxSubScore

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,9 +2,10 @@ from Easy.BestTimeBuySellStock import BuySellStock
 from Easy.ValidPalindrome import ValidPalindrome
 from Medium.CombinationSumIII import CombinationSumIII
 from Medium.LetterCombinationsPhoneNumber import LetterCombinationsPhoneNumber
+from Medium.MaxSubScore import MaxSubScore
 from Medium.SmallestInfiniteSet import SmallestInfiniteSet
 from TreeNode import TreeNodeHelper
 
 if __name__ == '__main__':
-    main = BuySellStock([7,1,5,3,6,4])
+    main = MaxSubScore([1,3,3,2], [2,1,3,4], 3)
 
