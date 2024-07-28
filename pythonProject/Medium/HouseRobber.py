8b8c662a67ae58b4120df7dafd95c149dc463b98
Bimzy <branden.immerzeel@gmail.com>
2024-07-28 21:04:38 +0930

    house robber

--- pythonProject/Medium/HouseRobber.py
+++ pythonProject/Medium/HouseRobber.py
@@ -9,4 +9,13 @@ class HouseRobber:
         LogHelper.PrintOutput(self.rob(nums))
 
     def rob(self, nums: List[int]) -> int:
-        
+        nLen = len(nums)
+
+        if nLen == 1:
+            return nums[0]
+
+        nums[1] = max(nums[0], nums[1])
+
+        for i in range(2, nLen):
+            nums[i] = max(nums[i - 1], nums[i - 2] + nums[i])
+        return nums[-1]
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,7 @@
 from Easy.BestTimeBuySellStock import BuySellStock
 from Easy.ValidPalindrome import ValidPalindrome
 from Medium.CombinationSumIII import CombinationSumIII
+from Medium.HouseRobber import HouseRobber
 from Medium.LetterCombinationsPhoneNumber import LetterCombinationsPhoneNumber
 from Medium.MaxSubScore import MaxSubScore
 from Medium.SmallestInfiniteSet import SmallestInfiniteSet
@@ -8,4 +9,4 @@ from Medium.TotalCostKWorkers import TotalCostKWorkers
 from TreeNode import TreeNodeHelper
 
 if __name__ == '__main__':
-    main = CombinationSumIII(3, 9)
+    main = HouseRobber([2,7,9,3,1])
